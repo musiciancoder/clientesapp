880f0188f32b96f6bdee0b5d3b9e7122e0202710
musiciancoder <andresguitarra1980@gmail.com>
2020-06-13 17:04:02 -0400

    19. Implementando rutas en Angular y navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,29 @@ import {FooterComponent} from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import {ClienteService} from './clientes/cliente.service';
+import { RouterModule, Routes} from '@angular/router';
+
+// ROUTING
+const  routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'}, //  full es toda la ruta
+  {path: 'directivas', component: DirectivaComponent},
+  {path: 'clientes', component: ClientesComponent}
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent, //
+  declarations: [  // COMPONENTES
+    AppComponent,
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
     ClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)  //ROUTING
   ],
-  providers: [ClienteService],
+  providers: [ClienteService], // CLASES SERVICIO
   bootstrap: [AppComponent]
 })
 export class AppModule { }
