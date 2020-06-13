281a97aaf92175b686ee769c224101ddb5b83a81
musiciancoder <andresguitarra1980@gmail.com>
2020-06-12 21:39:47 -0400

    11. *ngIf

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivaComponent {
 
+  //listaCurso y habilitar no son campos de clase como en Java, son atributos de la clase
+
   listaCurso:string[]=['Typescript','Javascript','Java SE', 'C#', 'PHP'];
+
+  habilitar: boolean = true;
 
   constructor() { }
 
-
+setHabilitar(): void { //metodo usado al hacer click en boton ocultar
+  this.habilitar =(this.habilitar==true)? false : true
+}
 
 }
