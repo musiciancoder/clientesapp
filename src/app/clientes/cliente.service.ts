29ed67585e9104df40fbf28bcab90baceeae3cdd
musiciancoder <andresguitarra1980@gmail.com>
2020-06-13 16:04:50 -0400

    18. implementando Observable...

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json'; //EL ARRAY
 import { Cliente} from './cliente';
-
+//import { Observable } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 @Injectable()
 export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente[] {
-    return CLIENTES; //devuelve el array
+  //El observable siempre es algun objeto del cliente. Existen Observadores, que son elementos del backend que se suscriben a este observable, para que
+  //cuando cambie algo en algun observador(backend) se vea reflejado en el observable(frontend)
+
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES); //con el metodo 'of' se pasa el array de objetos a un observable (tambien llamado sujeto observable), que en este caso seria el array Cliente[]
   }
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -11,11 +11,15 @@ import {ClienteService} from './cliente.service';
 export class ClientesComponent implements OnInit {
 
   clientes: Cliente[]; //El array que importamos lo definimos como atributo de clase
-  constructor(private clienteService: ClienteService){} //ver explicacion en Typescript.txt
+  constructor(private clienteService: ClienteService){} //Esta es inyeccion por dependencia por constructor. ver explicacion en Typescript.txt
 
 
   ngOnInit(): void {
-    this.clientes = this.clienteService.getClientes();//¿Por qué necesitaste decir que el array Cliente[] es parte de la clase con this.clientes = CLIENTES;? Acaso si no lo especificas en el método no lo reconoce al inicializar la app?
+    this.clienteService.getClientes().subscribe(
+      clientes => this.clientes = clientes //esto en Typescript es funcion anonima function(clientes) => { this.clientes = clientes }
+    );// this.clienteService.getClientes() es la ejecucion de inyeccion por dependencias
+    //con el metodo subscribe subscribimos el observable this.clienteService.getClientes() a un observador que se pasa como argumento
+    // ¿Por qué necesitaste decir que el array Cliente[] es parte de la clase con this.clientes = CLIENTES;? Acaso si no lo especificas en el método no lo reconoce al inicializar la app?
   }
 
 }
