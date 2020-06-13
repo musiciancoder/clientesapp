3c1f64c9e4b7d3627fd9da13953fc0b86d5bb479
musiciancoder <andresguitarra1980@gmail.com>
2020-06-13 12:20:12 -0400

    16. creando archivo clientes.json con la lista de objetos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
+import {ClIENTES} from './clientes.json'; //EL ARRAY
 
 
 @Component({
@@ -9,25 +10,13 @@ import {Cliente} from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1,nombre:'Andres', apellido:'Guzman' , email:'profesor@bolsa', createAt: '2017-12-11'},
-    {id: 2,nombre:'Marcelo', apellido:'Salas' , email:'matador@gmail', createAt: '2017-12-14'},
-    {id: 3,nombre:'Ivan', apellido:'Zamorano' , email:'bambam@gmail', createAt: '2017-12-15'},
-    {id: 4,nombre:'Juan_Carlos', apellido:'Letelier' , email:'llanero@gmail', createAt: '2017-12-15'},
-    {id: 5,nombre:'Patricio', apellido:'Yañez' , email:'patoyañez@gmail', createAt: '2017-12-16'},
-    {id: 6,nombre:'Carlos', apellido:'Caszely' , email:'chino@gmail', createAt: '2017-12-17'},
-    {id: 7,nombre:'Eduardo', apellido:'Vargas' , email:'edu@gmail', createAt: '2017-12-18'},
-    {id: 8,nombre:'Alexis', apellido:'Sanchez' , email:'maravilla@gmail', createAt: '2017-12-19'},
-    {id: 9,nombre:'Leonel', apellido:'Sanchez' , email:'leonel@gmail', createAt: '2017-12-21'},
-    {id: 10,nombre:'Ruben', apellido:'Martinez' , email:'ruben@gmail', createAt: '2017-12-21'},
-    {id: 11,nombre:'Hugo', apellido:'Rubio' , email:'pajaro@gmail', createAt: '2017-12-21'},
-
-  ]
+  clientes: Cliente[]; //El array que importamos lo definimos como atributo de clase
 
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = ClIENTES;//¿Por qué necesitaste decir que el array Cliente[] es parte de la clase con this.clientes = CLIENTES;? Acaso si no lo especificas en el método no lo reconoce al inicializar la app?
   }
 
 }
