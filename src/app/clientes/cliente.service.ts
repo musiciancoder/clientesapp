774cdb371763b5336e244e0ed92705157d99904d
musiciancoder <andresguitarra1980@gmail.com>
2020-06-15 18:28:49 -0400

    Escribiendo implementacion crear cliente en form.component.ts y cliente.service.ts

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json'; //EL ARRAY
 import { Cliente} from './cliente';
 import { of, Observable } from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
 
   private urlEndPoint:string = 'http://localhost:8080/api/clientes'; //esto es atributo de clase, por algo despues lo llama con this
+
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
 
   constructor(private http: HttpClient) { //INYECCION DE DEPENDENCIAS, CON ESTO http: HttpClient queda definida como atributo de la clase
 
@@ -17,6 +19,7 @@ export class ClienteService {
   //El observable siempre es algun objeto del cliente. Existen Observadores, que son elementos del backend que se suscriben a este observable, para que
   //cuando cambie algo en algun observador(backend) se vea reflejado en el observable(frontend)
 
+  //PARA OBTENER LA LISTA DE CLIENTES
   getClientes(): Observable<Cliente[]> {
     //return of(CLIENTES); //con el metodo 'of' se pasa el array de objetos a un observable (tambien llamado sujeto observable), que en este caso seria el array Cliente[]
     return this.http.get<Cliente[]>(this.urlEndPoint); //Primera forma de obtener un observable
@@ -24,4 +27,10 @@ export class ClienteService {
     map ((response) => response as Cliente[] ) //la respuesta la obtengo como un listado de clientes
   );*/
   }
+
+  //PARA CREAR UN CLIENTE
+  create(cliente:Cliente) : Observable<Cliente>{
+    return  this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
+  }
+
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
+import {ClienteService} from './cliente.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -10,15 +12,18 @@ export class FormComponent implements OnInit {
   public cliente:Cliente=new Cliente(); //a este atributo es donde se mapean los ngModel de los input
   public titulo:string = " Crear cliente";
 
-  constructor() { }
+  constructor(private clienteService : ClienteService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
   //Al apretar el boton de enviar el formulario
   public create():void{
-    console.log("Clicked");
-    console.log(this.cliente);
+    this.clienteService.create(this.cliente).subscribe( // Observable
+      response => this.router.navigate(['/clientes'])
+    )
+
   }
 
 }
