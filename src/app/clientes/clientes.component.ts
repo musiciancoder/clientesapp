e83bf9d94411cebe91564a0268ede246a3d83c09
musiciancoder <andresguitarra1980@gmail.com>
2020-06-22 16:47:18 -0400

    79. Agregando la ruta page del paginador y el Observable ActivatedRoute. Uso de paramsMap para obtener pagina de una url activa

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -3,6 +3,7 @@ import {Cliente} from './cliente';
 import {ClienteService} from './cliente.service';
 import swal from 'sweetalert2';
 import {tap} from 'rxjs/operators';
+import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
@@ -13,24 +14,34 @@ import {tap} from 'rxjs/operators';
 export class ClientesComponent implements OnInit {
 
   clientes: Cliente[]; //El array que importamos lo definimos como atributo de clase
-  constructor(private clienteService: ClienteService) {
-  } //Esta es inyeccion por dependencia por constructor. ver explicacion en Typescript.txt
+
+  constructor(private clienteService: ClienteService, //Esta es inyeccion por dependencia por constructor. ver explicacion en Typescript.txt
+              private activatedRoute: ActivatedRoute) {
+  }
 
 
   ngOnInit() {
 
+    this.activatedRoute.paramMap.subscribe(params => { // con paramMap convertimos a Observable
 
-  let page=0;
-    this.clienteService.getClientes(page).pipe(
-      tap(response => { //el tap no retorna nada, es un void.
-        console.log('ClienteService:tap3: ');
-        (response.content as Cliente[]).forEach(cliente => { //se usa forEach con tap, no map
-          console.log(cliente.nombre);
-        });
-      })
-    ).subscribe(response => this.clientes = response.content as Cliente[]);// this.clienteService.getClientes() es la ejecucion de inyeccion por dependencias
-    //con el metodo subscribe subscribimos el observable this.clienteService.getClientes() a un observador que se pasa como argumento
-    // ¿Por qué necesitaste decir que el array Cliente[] es parte de la clase con this.clientes = CLIENTES;? Acaso si no lo especificas en el método no lo reconoce al inicializar la app?
+      let page: number = +params.get('page'); //operador + transforma un string a un number. Con esto extraemos la pagina del url activo
+
+        if (!page) {
+          page = 0;
+        }
+
+        this.clienteService.getClientes(page).pipe(
+          tap(response => { //el tap no retorna nada, es un void.
+            console.log('ClienteService:tap3: ');
+            (response.content as Cliente[]).forEach(cliente => { //se usa forEach con tap, no map
+              console.log(cliente.nombre);
+            });
+          })
+        ).subscribe(response => this.clientes = response.content as Cliente[]);// this.clienteService.getClientes() es la ejecucion de inyeccion por dependencias
+        //con el metodo subscribe subscribimos el observable this.clienteService.getClientes() a un observador que se pasa como argumento
+        // ¿Por qué necesitaste decir que el array Cliente[] es parte de la clase con this.clientes = CLIENTES;? Acaso si no lo especificas en el método no lo reconoce al inicializar la app?
+      }
+    );
   }
 
   //Al presionar boton rojo eliminar
