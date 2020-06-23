c07471aeeb843840bad17da80f214cf41addf544
musiciancoder <andresguitarra1980@gmail.com>
2020-06-23 15:34:17 -0400

    90. Añadiendo DatePicker con Angular Material (ACA EL DISEÑO DEL FORMULARIO PARA CREAR SE VA A LA CHUCHA)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, LOCALE_ID} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
-import { DirectivaComponent } from './directiva/directiva.component';
-import { ClientesComponent } from './clientes/clientes.component';
-import { FormComponent } from './clientes/form.component';
-import { PaginatorComponent } from './paginator/paginator.component';
+import {DirectivaComponent} from './directiva/directiva.component';
+import {ClientesComponent} from './clientes/clientes.component';
+import {FormComponent} from './clientes/form.component';
+import {PaginatorComponent} from './paginator/paginator.component';
 
 import {ClienteService} from './clientes/cliente.service';
-import { RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 
+registerLocaleData(localeES, 'es'); //fecha en español
 import {FormsModule} from '@angular/forms';
 import {registerLocaleData} from '@angular/common';
-import  localeES from '@angular/common/locales/es';
+import localeES from '@angular/common/locales/es';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
 
-
-registerLocaleData(localeES, 'es'); //fecha en español
 
 // ROUTING
-const  routes: Routes = [
+const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'}, //  full es toda la ruta
   {path: 'directivas', component: DirectivaComponent},
   {path: 'clientes', component: ClientesComponent},
   {path: 'clientes/page/:page', component: ClientesComponent},
-  {path: 'clientes/form', component:FormComponent},
-  {path: 'clientes/form/:id', component:FormComponent}
-  ];
+  {path: 'clientes/form', component: FormComponent},
+  {path: 'clientes/form/:id', component: FormComponent}
+];
 
 
 @NgModule({
@@ -45,10 +47,14 @@ const  routes: Routes = [
     BrowserModule,
     HttpClientModule, //PARA CONECTAR CON EL BACKEND DE SPRING
     FormsModule, // para trabajar con formularios
-    RouterModule.forRoot(routes)  //ROUTING
+    RouterModule.forRoot(routes),  //ROUTING
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatMomentDateModule
   ],
   providers: [ClienteService, // CLASES SERVICIO
-{provide: LOCALE_ID, useValue: 'es'}], //para pasar a formato fecha en español en los html
+    {provide: LOCALE_ID, useValue: 'es'}], //para pasar a formato fecha en español en los html
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
