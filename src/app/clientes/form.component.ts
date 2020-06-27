7cffea629960edb725afe6b61e85853097939134
musiciancoder <andresguitarra1980@gmail.com>
2020-06-27 15:50:56 -0400

    114. Añadiendo opción por defecto con el texto seleccionar

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -78,6 +78,10 @@ export class FormComponent implements OnInit {
   }
 
   compararRegion(o1:Region, o2:Region): boolean{ // o1 y o2 son objetos
+  if (o1 === undefined && o2 === undefined){
+    return true;
+  }
+
     return o1 ===null || o2 ===null || o1 ===undefined || o2 ===undefined? false: o1.id===o2.id;
   }
 
