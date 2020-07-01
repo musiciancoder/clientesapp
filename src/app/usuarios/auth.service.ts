5e372e0e2cf22f6cf039c508ac5ad1344380550d
musiciancoder <andresguitarra1980@gmail.com>
2020-07-01 15:07:33 -0400

    140. Creando la clase de servicio AuthService y su método login()

---
+++ src/app/usuarios/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Usuario} from './usuario';
+import {HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//CLASE SERVICIO PARA ENVIAR DATOS DE USUARIO AL BACKEND
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+
+  login(usuario:Usuario):Observable<any>{
+    const urlEndpoint = 'http://localhost:8080/oauth/token';
+    const credenciales = btoa('angularapp' + ':' + '12345'); //encriptar base 64
+    const httpHeaders = new HttpHeaders({'Content-Type':'application/x-www-form-urlencoded',
+    'Authorization':'Basic ' + credenciales});
+
+    let params = new URLSearchParams();
+    params.set('grant_type','password');
+    params.set('username',usuario.username);
+    params.set('password', usuario.password);
+
+    return this.http.post<any>(urlEndpoint, params, {headers: httpHeaders})
+
+  }
+
+}
